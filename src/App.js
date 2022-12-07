b14c79a190fb3e2bcbfc9c07b2d312671d65cb32
Ali Karabulut <a.kbt6666@gmail.com>
2022-12-08 01:10:10 +0300

    offset

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [position, setPosition] = useState({});
+
+  const clickHandle = (e) => {
+    const {top, width, height } = e.target.getBoundingClientRect();
+    // navbar ekranın ortasından yarını kaplıyo diyelim o zaman bu left değeri bozulacak
+    // çünkü bu komple ekrana göre alıyor
+    // bu yüzden lefti offsetleft ile alcaz
+    // bu sayede kapsayıcıya göre left değeri almış olcaz
+
+    const left = e.target.offsetLeft
+    setPosition({
+      left,
+      top,
+      width,
+      height,
+    });
+  };
+  // getBoundingClientRect() = > x y h w olarak
+  // bütün değerleri ayrıntılı olarak verir
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="menu">
+        {Object.values(position).length > 0 && (
+          <div
+            className="divider"
+            style={{
+              "--left": position.left + "px",
+              "--top": position.top + "px",
+              "--width": position.width + "px",
+              "--height": position.height + "px",
+            }}
+          ></div>
+        )}
+        <button onClick={clickHandle}>Hakkımda</button>
+        <button onClick={clickHandle}>iletişim</button>
+        <button onClick={clickHandle}>Blog</button>
+        <button onClick={clickHandle}>Makaleler</button>
+        <button onClick={clickHandle}>Dersler</button>
+      </nav>
     </div>
   );
 }
